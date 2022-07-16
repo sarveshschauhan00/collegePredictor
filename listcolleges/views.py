368724defb6b1a16a8b2c80f03dbb8c566b85df1
sarvesh <sarvesh.s.chauhan.00@gmail.com>
2022-07-16 20:27:20 +0530

    search filture is added

--- listcolleges/views.py
+++ listcolleges/views.py
@@ -4,6 +4,9 @@ from .models import RankTable
 from .forms import RankForm
 import csv
 
+def redirect_site(request):
+    return redirect('/', permanent=True)
+
 
 # def index(request):
 #     rank_form = None
